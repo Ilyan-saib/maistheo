ff0f7e3026eced7f3b09395b6dbe97c3e8182f53
Ilyan-saib <isaibmezghiche@edenschool.fr>
2022-12-13 21:58:16 +0100

    sovscode

--- main.js
+++ main.js
@@ -1,41 +1,26 @@
 const config = import.meta.env
 let submit = document.getElementById("btn")
 let input = document.getElementById('formulaire')
-const sectContainer = document.getElementById("sect-container")
+let sectContainer = document.getElementById("sect-container")
 
 let town;
 
 
 
-
-
-
-input.addEventListener('focus', () => {
-  input.value = ""
-})
-
-
-
-
-submit.addEventListener('click', () => {
-
-  town = input.value
-
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=${config.VITE_API_KEY}&units=metric`)
-    .then((response) => response.json())
-    .then((dta) => console.log(dta))
-
-  input.value = "Search for a city"
-
-
+function CreateElm(data) {
   let sect = document.createElement('section')
   sect.classList.add('sect')
 
-  let info_title = document.createElement('p')
-  info_title.classList.add('town')
 
+ let info_title = document.createElement('p')
+  info_title.classList.add('town')
+  info_title.innerText =  data.name + ` `
+  
   let info_country = document.createElement('div')
   info_country.classList.add('pays')
+ info_country =  data.sys.country
+
+
 
   let pic = document.createElement('img')
   pic.classList.add('icon')
@@ -44,11 +29,65 @@ submit.addEventListener('click', () => {
   info_desc.classList.add('desc')
 
   sectContainer.appendChild(sect)
-  sect.appendChild(info_title)
   sect.appendChild(info_country)
+  sect.appendChild(info_title)
   sect.appendChild(info_desc)
   sect.appendChild(pic)
+}
+
+
+input.addEventListener('focus', () => {
+  input.value = ""
 })
 
+
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+   town = input.value     
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=${config.VITE_API_KEY}&units=metric`)
+    .then((response) => response.json())
+    .then((dta) => {
+      if (dta.cod == 404 || dta.cod == 400 ) { 
+        console.log('cfo');
+       }
+       else{
+        CreateElm(dta);
+       }  
+    })
+
+  input.value = ""
+
+
+
+
+
+}
+  }
+  )
+
+ submit.addEventListener("click", ()=> {
+    town = input.value
+        
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=${config.VITE_API_KEY}&units=metric`)
+    .then((response) => response.json())
+    .then((dta) => {
+       if (dta.cod == 404 || dta.cod == 400) { 
+        console.log('cfo');
+       }
+       else{
+        CreateElm();
+       }
+    })
+
+  input.value = "Search for a city"
+
+
+    
+
+
+}
+  
+  )
 
 
