f0d8267098aaf77df65c54681fd0d3481777d183
CannelleJP <isaibmezghiche@edenschool.fr>
2022-12-13 17:16:07 +0100

    c easy

--- main.js
+++ main.js
@@ -1,53 +1,54 @@
- const config = import.meta.env
+const config = import.meta.env
 let submit = document.getElementById("btn")
 let input = document.getElementById('formulaire')
+const sectContainer = document.getElementById("sect-container")
 
- let town;
- 
-
-
+let town;
 
 
- 
-input.addEventListener('focus', ()=>{
-input.value = ""
+
+
+
+
+input.addEventListener('focus', () => {
+  input.value = ""
 })
 
 
 
 
-submit.addEventListener('click',()=>{
+submit.addEventListener('click', () => {
 
   town = input.value
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=${config.VITE_API_KEY}&units=metric`)
-  .then((response) => response.json())
-  .then((dta) => console.log(dta))
+    .then((response) => response.json())
+    .then((dta) => console.log(dta))
 
   input.value = "Search for a city"
 
 
-let sect = document.createElement('section')
-sect.classList.add('sect')
+  let sect = document.createElement('section')
+  sect.classList.add('sect')
 
-let info_title = document.createElement('p')
-info_title.classList.add('town')
+  let info_title = document.createElement('p')
+  info_title.classList.add('town')
 
-let info_country = document.createElement('div')
-info_country.classList.add('pays')
+  let info_country = document.createElement('div')
+  info_country.classList.add('pays')
 
-let pic = document.createElement('img')
-pic.classList.add('icon')
+  let pic = document.createElement('img')
+  pic.classList.add('icon')
 
-let info_desc = document.createElement('p') 
-info_desc.classList.add('desc')
+  let info_desc = document.createElement('p')
+  info_desc.classList.add('desc')
 
-document.body.appendChild(sect)
-sect.appendChild(info_title)
-sect.appendChild(info_country)
-sect.appendChild(info_desc)
-sect.appendChild(pic)
+  sectContainer.appendChild(sect)
+  sect.appendChild(info_title)
+  sect.appendChild(info_country)
+  sect.appendChild(info_desc)
+  sect.appendChild(pic)
 })
- 
+
 
 
